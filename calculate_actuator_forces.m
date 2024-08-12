b1bc9b87a57757bcfa39798a2dfdafb91d19d173
ardakorsan9 <ardakorsan9@hotmail.com>
2024-08-12 18:19:58 +0300

    .
    .

---
+++ calculate_actuator_forces.m
@@ -0,0 +1,13 @@
+function actuator_forces = calculate_actuator_forces(position_control, orientation_control)
+    global K_t K_m tau_m
+
+    % Example actuator dynamics
+    force_x = K_t * tanh(K_m * position_control(1) / tau_m);
+    force_y = K_t * tanh(K_m * position_control(2) / tau_m);
+    force_z = K_t * tanh(K_m * position_control(3) / tau_m);
+    moment_k = K_t * tanh(K_m * orientation_control(1) / tau_m);
+    moment_m = K_t * tanh(K_m * orientation_control(2) / tau_m);
+    moment_n = K_t * tanh(K_m * orientation_control(3) / tau_m);
+
+    actuator_forces = [force_x; force_y; force_z; moment_k; moment_m; moment_n];
+end
