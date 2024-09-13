ef0a8c3cc2389d67d722f6072d8d469f2599da08
ardakorsan9 <ardakorsan9@hotmail.com>
2024-09-13 04:51:17 +0300

    .
    .

---
+++ run_simulation_to_waypoint.m
@@ -0,0 +1,24 @@
+function vehicle_path = run_simulation_to_waypoint(current_waypoint, f_0, dt, total_time, vehicle_path, times, velocities, angular_velocities, fig1, fig2, h_vehicle_path, h_velocity, h_angular_velocity, h_position)
+    % Waypoint'e ulaşana kadar simülasyonu çalıştır
+    distance_to_waypoint = norm(current_waypoint - f_0(1:3)); 
+    while distance_to_waypoint > 0.5
+        t_span = total_time:dt:(total_time + dt);
+
+        % Anlık çözüm (ODE45)
+        [~, g] = ode45(@(t, g) underwater_vehicle_dynamics(t, g, current_waypoint, [0; 0; 0]), t_span, f_0);
+
+        % Verileri güncelle
+        vehicle_path = [vehicle_path; g(end, 1:3)];
+        times = [times; t_span(end)];
+        velocities = [velocities; g(end, 7:9)];
+        angular_velocities = [angular_velocities; g(end, 10:12)];
+
+        % Gerçek zamanlı grafikleri güncelle
+        update_real_time_plots(fig1, fig2, vehicle_path, times, velocities, angular_velocities, h_vehicle_path, h_velocity, h_angular_velocity, h_position);
+
+        % Son durumu bir sonraki adıma geçiş için başlat
+        f_0 = g(end, :)';
+        distance_to_waypoint = norm(current_waypoint - f_0(1:3));
+        total_time = total_time + dt;
+    end
+end
--- underwater_vehicle_simulation.m
+++ underwater_vehicle_simulation.m
@@ -6,7 +6,7 @@ function underwater_vehicle_simulation()
     waypoints = [40, 5, -10; 
                  25, 20, -15; 
                  30, 25, -20; 
-                 40, 30, -25]; % Waypoints listesi
+                 40, 30, -25]; 
 
     % Başlangıç pozisyonu
     f_0 = zeros(12, 1); 
@@ -15,48 +15,19 @@ function underwater_vehicle_simulation()
     dt = 0.2; % Zaman adımı
     t_total = 20; % Toplam simülasyon süresi
 
-    % Boş figürleri hazırla (smooth ve anlık güncellenebilir olacak)
-    [fig1, fig2, h_vehicle_path, h_velocity, h_angular_velocity, h_position] = setup_plots(waypoints); % Waypoint grafiği ve hız/pozisyon grafiği
+    % Grafikleri hazırla
+    [fig1, fig2, h_vehicle_path, h_velocity, h_angular_velocity, h_position] = setup_plots(waypoints);
     
     % Araç yolu, hız ve açısal hız verileri için boş diziler
-    vehicle_path = [];  % X, Y, Z pozisyonları
-    times = [];  % Zamanları kaydet
-    velocities = [];  % Hız verileri (u, v, w)
-    angular_velocities = [];  % Açısal hızlar (p, q, r)
+    vehicle_path = [];
+    times = [];
+    velocities = [];
+    angular_velocities = [];
 
-    % Her bir waypoint için simülasyon yapıyoruz
-    total_time = 0; % Toplam simülasyon süresi (uç uca ekleyeceğiz)
-
+    % Her bir waypoint için simülasyonu çalıştır
+    total_time = 0;
     for waypoint_index = 1:size(waypoints, 1)
         current_waypoint = waypoints(waypoint_index, :)';
-        distance_to_waypoint = norm(current_waypoint - f_0(1:3));  % Mevcut pozisyon ile waypoint arası mesafe
-
-        % Simülasyonu zaman adımlarıyla yürüt ve grafikleri her adımda güncelle
-        t_idx = 0;  % Zaman indeksi
-        while distance_to_waypoint > 0.5  % Waypoint'e ulaşana kadar çalıştır
-            t_span = total_time:dt:(total_time + dt); % Her adımda sadece dt kadar ilerle
-            
-            % Anlık çözüm (ODE45)
-            [~, g] = ode45(@(t, g) underwater_vehicle_dynamics(t, g, current_waypoint, [0; 0; 0]), t_span, f_0);
-
-            % Verileri kaydet
-            vehicle_path = [vehicle_path; g(end, 1:3)];  % X, Y, Z pozisyonları
-            times = [times; t_span(end)];  % Zamanı kaydet
-            velocities = [velocities; g(end, 7:9)];  % Hız (u, v, w)
-            angular_velocities = [angular_velocities; g(end, 10:12)];  % Açısal hızlar (p, q, r)
-
-            % Gerçek zamanlı olarak grafikleri güncelle
-            update_real_time_plots(fig1, fig2, vehicle_path, times, velocities, angular_velocities, h_vehicle_path, h_velocity, h_angular_velocity, h_position);
-
-            % Son durumu bir sonraki adıma geçiş için başlat
-            f_0 = g(end, :)';  % Yeni başlangıç durumu
-
-            % Mesafeyi güncelle (waypoint'e yaklaştık mı kontrol edelim)
-            distance_to_waypoint = norm(current_waypoint - f_0(1:3));
-
-            % Toplam simülasyon süresini güncelle
-            total_time = total_time + dt;
-            t_idx = t_idx + 1;
-        end
+        vehicle_path = run_simulation_to_waypoint(current_waypoint, f_0, dt, total_time, vehicle_path, times, velocities, angular_velocities, fig1, fig2, h_vehicle_path, h_velocity, h_angular_velocity, h_position);
     end
 end
