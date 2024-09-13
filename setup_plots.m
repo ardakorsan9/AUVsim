19f2d521b6da8160185e4ca5629ea502405ced55
ardakorsan9 <ardakorsan9@hotmail.com>
2024-09-13 03:52:30 +0300

    .
    .

--- setup_plots.m
+++ setup_plots.m
@@ -1,25 +1,41 @@
-function [fig1, fig2] = setup_plots(waypoints)
-    % Anlık güncellemeler için grafik figürlerini hazırlama
-
+function [fig1, fig2, h_vehicle_path, h_velocity, h_angular_velocity, h_position] = setup_plots(waypoints)
     % Waypoints ve araç yolu için figür
     fig1 = figure(1);
     plot3(waypoints(:, 1), waypoints(:, 2), waypoints(:, 3), 'ro', 'MarkerSize', 8, 'MarkerFaceColor', 'r');
     hold on;
+    h_vehicle_path = plot3(nan, nan, nan, 'b-', 'LineWidth', 1);  % Boş bir plot, sadece güncellenecek
     xlabel('X Pozisyonu (m)');
     ylabel('Y Pozisyonu (m)');
     zlabel('Z Pozisyonu (m)');
     grid on;
     axis equal;
     title('AUV Waypoint Takibi');
-    
+    legend('Waypoints', 'AUV Yolu');  % Sadece bir kere legend ekle
+
     % Hız, açısal hız ve pozisyon için figür
     fig2 = figure(2);
+    
+    % Hız grafiği (u, v, w)
     subplot(3, 1, 1); hold on;
+    h_velocity(1) = plot(nan, nan, 'b');  % u
+    h_velocity(2) = plot(nan, nan, 'g');  % v
+    h_velocity(3) = plot(nan, nan, 'r');  % w
     xlabel('Time (s)'); ylabel('Velocity (m/s)');
-    
+    legend('u', 'v', 'w');  % Legend sadece bir kez eklenir
+
+    % Açısal hız grafiği (p, q, r)
     subplot(3, 1, 2); hold on;
+    h_angular_velocity(1) = plot(nan, nan, 'b');  % p
+    h_angular_velocity(2) = plot(nan, nan, 'g');  % q
+    h_angular_velocity(3) = plot(nan, nan, 'r');  % r
     xlabel('Time (s)'); ylabel('Angular Velocity (rad/s)');
-    
+    legend('p', 'q', 'r');  % Legend sadece bir kez eklenir
+
+    % Pozisyon grafiği (x, y, z)
     subplot(3, 1, 3); hold on;
+    h_position(1) = plot(nan, nan, 'b');  % x
+    h_position(2) = plot(nan, nan, 'g');  % y
+    h_position(3) = plot(nan, nan, 'r');  % z
     xlabel('Time (s)'); ylabel('Position (m)');
+    legend('x', 'y', 'z');  % Legend sadece bir kez eklenir
 end
--- update_real_time_plots.m
+++ update_real_time_plots.m
@@ -1,23 +1,27 @@
-function update_real_time_plots(fig1, fig2, vehicle_path, waypoints, times, velocities, angular_velocities)
+function update_real_time_plots(fig1, fig2, vehicle_path, times, velocities, angular_velocities, h_vehicle_path, h_velocity, h_angular_velocity, h_position)
     % 3D araç yolunu güncelle
     figure(fig1);
-    plot3(vehicle_path(:, 1), vehicle_path(:, 2), vehicle_path(:, 3), 'b-', 'LineWidth', 1);
+    % Mevcut grafiği güncelle
+    set(h_vehicle_path, 'XData', vehicle_path(:, 1), 'YData', vehicle_path(:, 2), 'ZData', vehicle_path(:, 3));
     drawnow;
 
     % Hız, açısal hız ve pozisyon grafiğini güncelle
     figure(fig2);
     
     % Hız grafiği (u, v, w)
-    subplot(3, 1, 1);
-    plot(times, velocities(:, 1), 'b', times, velocities(:, 2), 'g', times, velocities(:, 3), 'r');
+    set(h_velocity(1), 'XData', times, 'YData', velocities(:, 1));  % u
+    set(h_velocity(2), 'XData', times, 'YData', velocities(:, 2));  % v
+    set(h_velocity(3), 'XData', times, 'YData', velocities(:, 3));  % w
     
     % Açısal hız grafiği (p, q, r)
-    subplot(3, 1, 2);
-    plot(times, angular_velocities(:, 1), 'b', times, angular_velocities(:, 2), 'g', times, angular_velocities(:, 3), 'r');
+    set(h_angular_velocity(1), 'XData', times, 'YData', angular_velocities(:, 1));  % p
+    set(h_angular_velocity(2), 'XData', times, 'YData', angular_velocities(:, 2));  % q
+    set(h_angular_velocity(3), 'XData', times, 'YData', angular_velocities(:, 3));  % r
     
     % Pozisyon grafiği (x, y, z)
-    subplot(3, 1, 3);
-    plot(times, vehicle_path(:, 1), 'b', times, vehicle_path(:, 2), 'g', times, vehicle_path(:, 3), 'r');
+    set(h_position(1), 'XData', times, 'YData', vehicle_path(:, 1));  % x
+    set(h_position(2), 'XData', times, 'YData', vehicle_path(:, 2));  % y
+    set(h_position(3), 'XData', times, 'YData', vehicle_path(:, 3));  % z
     
     drawnow;
 end
