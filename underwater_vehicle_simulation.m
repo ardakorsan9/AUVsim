85e2f03bc10160db6cecc2db990e7224bc98b78c
ardakorsan9 <ardakorsan9@hotmail.com>
2024-09-17 16:07:36 +0300

    .
    .

--- underwater_vehicle_simulation.m
+++ underwater_vehicle_simulation.m
@@ -3,7 +3,7 @@ function underwater_vehicle_simulation()
     init_parameters();
 
     % Waypoints tanımlama
-    waypoints = [2, 3, -1; 
+    waypoints = [40, 5, -10; 
                  25, 20, -15; 
                  30, 25, -20; 
                  40, 30, -25]; 
@@ -14,7 +14,7 @@ function underwater_vehicle_simulation()
     % Simülasyon zamanı
     dt = 0.2; % Zaman adımı
 
-    % Grafikleri hazırla
+    % Boş figürleri hazırla
     [fig1, fig2, h_vehicle_path, h_velocity, h_angular_velocity, h_position] = setup_plots(waypoints);
     
     % Araç yolu, hız ve açısal hız verileri için boş diziler
@@ -25,8 +25,14 @@ function underwater_vehicle_simulation()
 
     % Her bir waypoint için simülasyonu çalıştır
     total_time = 0;
+
     for waypoint_index = 1:size(waypoints, 1)
         current_waypoint = waypoints(waypoint_index, :)';
-        [vehicle_path, times, velocities, angular_velocities, total_time] = run_simulation_to_waypoint(current_waypoint, f_0, dt, total_time, vehicle_path, times, velocities, angular_velocities, fig1, fig2, h_vehicle_path, h_velocity, h_angular_velocity, h_position);
+        
+        % Simülasyonu çalıştır ve sonuçları güncelle
+        [vehicle_path, times, velocities, angular_velocities, total_time, f_0] = ...
+            run_simulation_to_waypoint(current_waypoint, f_0, dt, total_time, ...
+            vehicle_path, times, velocities, angular_velocities, ...
+            fig1, fig2, h_vehicle_path, h_velocity, h_angular_velocity, h_position);
     end
 end
